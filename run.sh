ea350dc9778a850398d674a95157525c80260b25
Irene Diez <idiez@redhat.com>
2023-12-18 16:19:56 +0100

    Change quay repo and virt-install commands
    Signed-off-by: Irene Diez <idiez@redhat.com>

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ if [ ! -f "$FILE" ]; then
 fi
 virt-install --connect qemu:///system --name sagano-demo --memory 2048 --vcpus 4 --disk size=40 \
           --boot loader=/usr/share/edk2/ovmf/OVMF_CODE.secboot.fd,loader.readonly=yes,loader.secure='no',loader.type=pflash,nvram=/var/lib/libvirt/qemu/nvram/sagano-demo_VARS.fd --network=network=default,model=virtio \
-          --os-variant rhel9.0 --location boot.iso \
+          --os-variant rhel9.0 --location boot.iso --check disk_size=off \
           --noautoconsole --initrd-inject $(pwd)/example.ks --extra-args="inst.ks=file:/example.ks console=tty0 console=ttyS0,115200 inst.profile=rhel"
